a8adac53022c7d54daf97485cfde072a92b113bb
Christian Zamora <65132785+cris-zmra1991@users.noreply.github.com>
2021-11-26 13:47:01 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,17 +5,36 @@
     <title>Jardín Almendares</title>
 
     <link href="res/main.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   </head>
   <body>
     <div class="topb">
-      <div class="cont">
-        <div class="subcont"><img src="res/logo.png" width="120" height="120"></div>
-        <div>
-        <span class="title">Jardín Almendares</span>
-        <br>
-        <span class="subtitle">Plantas ornamentales</span>
-      </div>
+      <nav class="navbar">
+        <div class="container">
+              <div class="d-flex align-items-center">
+              <div class="subcont"><img src="res/logo.png" width="120" height="120"></div>
+              <div>
+              <span class="title">Jardín Almendares</span>
+              <br>
+              <span class="subtitle">Plantas ornamentales</span>
+            </div>
+            </div>
+            <div class="d-flex flex-column ">
+              <span>
+                <form class="d-flex">
+                  <input class="search-input" type="text" placeholder="Search">
+                  <label class="search-btn"><span class="fa fa-search search-icon"></span></label>
+                </form>
+              </span>
+              <div class="d-flex container-fluid mt-4 justify-content-center"><i class="fab fa-facebook icon"></i> &nbsp; &nbsp; &nbsp; <i class="fab fa-whatsapp icon"></i> &nbsp; &nbsp; &nbsp; <i class="fa fa-phone icon"></i> &nbsp; &nbsp; &nbsp; <i class="fa fa-user icon"></i></div>
+            </div>
+        </div>
+      </nav>
     </div>
+  <br>
+  <br>
+
   </body>
 </html>
