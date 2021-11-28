530b8d839b5f6ff844b61d514159a1881c66ccae
Christian Zamora <65132785+cris-zmra1991@users.noreply.github.com>
2021-11-28 15:51:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Jardín Almendares</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Jardin Almendares Plantas Ornamentales Cactus Suculentas">
     <link href="res/main.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
