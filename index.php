62b250ccb2f8006869b5c15a794eff5780b773c0
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-08 19:57:07 -0500

    Creado index y agregado cdn de bootstrap y font awesome

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ee85bfbf65.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
