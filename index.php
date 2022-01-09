90a15ab8c92210965e47fbf264b930cf4d73eb22
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-08 20:06:46 -0500

    Agregada barra de menu superior con Bootstrap

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             left: 0;
             right: 0;
             bottom: 0;
-            background-color: #000;
+            background-color: #FFFFFF;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -75,7 +75,31 @@
       });
     </script>
 
-    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">Hidden brand</a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
 
   </body>
 </html>
