1ba0c518f0fab9cffa8ea99cda091ace0132351d
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-08 20:32:09 -0500

    Cambios en los enlaces del menu

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="res/logo.png" alt="" width="35" height="35" class="d-inline-block align-text-top">
-          Bootstrap
+          Jardín Almendares
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -89,18 +89,18 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="#">Ofertas especiales</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="#">Biblioteca</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a class="nav-link href="#"">Contacto</a>
             </li>
           </ul>
           <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input class="form-control me-2" type="search" placeholder="Busque un producto" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Buscar</button>
           </form>
         </div>
       </div>
