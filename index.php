a4a8f479ee6bdeffe5b85081a2cd34436226c626
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-08 20:28:35 -0500

    Cambios en el logo

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="res/logo.png" alt="" width="40" height="40" class="d-inline-block align-middle">
+          <img src="res/logo.png" alt="" width="35" height="35" class="d-inline-block align-text-top">
           Bootstrap
         </a>
 
